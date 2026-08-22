e73f812b62729e850a53403fcb88a4acbdf4c3d4
marcel <marcel.hajek7@gmail.com>
2026-08-22 15:23:37 +0200

    fixed filename typo

--- AST/FielfNode.cs
+++
@@ -1,9 +0,0 @@
-namespace Marey.AST;
-
-internal sealed class FieldNode : ClassItem
-{
-    internal override void Parse(ParsePacket parsePacket)
-    {
-        throw new NotImplementedException();
-    }
-}
