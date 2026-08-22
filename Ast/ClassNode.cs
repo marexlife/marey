ec1dc32b3f29e6dd7bff2f5159fb5e2b341990fd
marcel <marcel.hajek7@gmail.com>
2026-08-22 16:36:28 +0200

    fixed more using

--- Ast/ClassNode.cs
+++ Ast/ClassNode.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Marey.Lex;
 
 namespace Marey.Ast;
 
--- Ast/FieldNode.cs
+++ Ast/FieldNode.cs
@@ -2,7 +2,7 @@ namespace Marey.Ast;
 
 internal sealed class FieldNode : ClassItem
 {
-    internal override void Parse(ParsePacket parsePacket)
+    internal override void Parse(ParsePacket packet)
     {
         throw new NotImplementedException();
     }
